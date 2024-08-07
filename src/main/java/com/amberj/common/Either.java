efd7bed9bf072c334faebce3f701e8dfe6a49e06
akashKarmakar02 <karmakar.akash1102@gmail.com>
2024-08-07 12:05:42 +0530

    misc: spelling fix

--- src/main/java/com/amberj/common/Either.java
+++ src/main/java/com/amberj/common/Either.java
@@ -4,24 +4,24 @@ import java.util.function.Consumer;
 
 
 public class Either<R, E> {
-    private final R response;
+    private final R data;
     private final E error;
 
-    private Either(R response, E error) {
-        this.response = response;
+    private Either(R data, E error) {
+        this.data = data;
         this.error = error;
     }
 
-    public static <R, E> Either<R, E> ofResponse(R response) {
-        return new Either<>(response, null);
+    public static <R, E> Either<R, E> ofData(R data) {
+        return new Either<>(data, null);
     }
 
     public static <R, E> Either<R, E> ofError(E error) {
         return new Either<>(null, error);
     }
 
-    public boolean isResponse() {
-        return response != null;
+    public boolean isData() {
+        return data != null;
     }
 
     public boolean isError() {
@@ -29,8 +29,8 @@ public class Either<R, E> {
     }
 
     public void get(Consumer<R> responseConsumer, Consumer<E> errorConsumer) {
-        if (isResponse()) {
-            responseConsumer.accept(response);
+        if (isData()) {
+            responseConsumer.accept(data);
         } else if (isError()) {
             errorConsumer.accept(error);
         }
